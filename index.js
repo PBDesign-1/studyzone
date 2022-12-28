704885115d0be61aa51926e832674270cd74ca0a
Philipp Binder <philipp.binder.design@gmail.com>
2022-12-28 14:55:41 +0100

    changes 28.12.2022 14:55

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const express = require('express')
 const path = require('path');
 const app = express();
 const cors = require("cors") //delete
+app.use(express.json());
 app.use(cors())
 require('dotenv').config();
 
--- routes/admin.js
+++ routes/admin.js
@@ -1,9 +1,9 @@
 const {Router, json} = require("express");
+const { ObjectId } = require("mongodb");
 const { tryCatch, authCheck } = require("../basic/shotcuts");
 const router = Router()
 const fetch = (...args) => import("node-fetch").then(({default: fetch}) => fetch(...args))
 const {connection} = require("../mongo/connection")
-connection.open()
 const db = connection.db.db("studyzone")
 
 router.get("/authCheck", (req, res)=>{
@@ -16,24 +16,34 @@ router.get("/authCheck", (req, res)=>{
 
 
 router.post("/stack", async (req, res)=>{
-    const {key, name, questionName, answerName} = req.body;
-    
+    console.log(req.body)
+
     tryCatch(res, async ()=>{
+        const {key, subjectID, name, questionName, answerName} = req.body;
+
         authCheck(res, key, async ()=>{
             let insert;
-            if([name, questionName, answerName].every(x=>!!x && typeof x == "string")){
-                insert = await db.collection("subject").insert({
-                    name,
-                    indexcards: [],
-                    questionName,
-                    answerName
-                });
+            if([name, questionName, answerName, subjectID].every(x=>!!x && typeof x == "string")){
+                const subject = await db.collection("subjects").findOne({_id: ObjectId(subjectID)})
+                const stackExists = !!subject.stacks.find(s=>s.name == name)
+
+                if(!stackExists){
+                    insert = await db.collection("subjects").updateOne(
+                        {_id: ObjectId(subjectID)}, 
+                        {$push: {
+                            stacks: {
+                                name,
+                                indexcards: [],
+                                questionName,
+                                answerName
+                            }
+                        }
+                    });                    
+                }
+
             }
     
-            res.json({success: true, response: !!insert ? {
-                
-            } : {} })
-     
+            res.json({success: !!insert ? insert.modifiedCount > 0 : false, response: {} })
         })  
     })
 })
@@ -41,10 +51,13 @@ router.post("/stack", async (req, res)=>{
 
 
 router.put("/stack", (req, res)=>{
-    const {key, subject, stack, changes} = req.body;
+    const {key, subjectID, stackName, changes} = req.body;
 
     tryCatch(res, async ()=>{
-        authCheck(res, key, ()=>{
+        authCheck(res, key, async ()=>{
+            const subject = await db.collection("subject").find({_id: subjectID})
+            const stackExists = subject.stacks.find(s=>s.name == stackName)
+
             res.json({success: true, response: {
                 
             }})            
