26d49adf7d18909e11d01de237c18ac94d287e16
Philipp Binder <philipp.binder.design@gmail.com>
2023-03-07 18:52:37 +0100

    07.03.2023 18:52

--- mongo/connection.js
+++ mongo/connection.js
@@ -2,19 +2,16 @@ const MongoClient = require('mongodb').MongoClient
 require('dotenv').config();
 
 class connection {
-
-    static async open() {
-        if (this.db) return this.db
-        this.db = await MongoClient.connect(this.url, this.options)
-        // console.log(await this.db.db("studyzone").collection("subjects").findOne({}))
-        return this.db
+  static async open() {
+    if (!this.db) {
+      this.db = await MongoClient.connect(this.url, this.options)
     }
-
+    return this.db.db("studyzone")
+  }
 }
 
 connection.db = null
 connection.url = process.env.Mongo_URI
-
-
+connection.options = { useNewUrlParser: true, useUnifiedTopology: true }
 
 module.exports = {connection};
